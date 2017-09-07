7f5302560424b9ff219dbf217801615fef3b1f38
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2017-09-07 13:54:57 +0300

    Revert "cmd/compile/internal/ppc64, cmd/compile/internal/ssa: Remove OldArch checks"
    This reverts commit c644a76e1f215d4ae2b210e7c9134b2291077ad3.

--- src/cmd/compile/internal/ssa/config.go
+++ src/cmd/compile/internal/ssa/config.go
@@ -39,6 +39,7 @@ type Config struct {
 	nacl            bool          // GOOS=nacl
 	use387          bool          // GO386=387
 	SoftFloat       bool          //
+	OldArch         bool          // True for older versions of architecture, e.g. true for PPC64BE, false for PPC64LE
 	NeedsFpScratch  bool          // No direct move between GP and FP register sets
 	BigEndian       bool          //
 	sparsePhiCutoff uint64        // Sparse phi location algorithm used above this #blocks*#variables score
@@ -254,6 +255,7 @@ func NewConfig(arch string, types Types, ctxt *obj.Link, optimize bool) *Config 
 		c.hasGReg = true
 		c.noDuffDevice = objabi.GOOS == "darwin" // darwin linker cannot handle BR26 reloc with non-zero addend
 	case "ppc64":
+		c.OldArch = true
 		c.BigEndian = true
 		fallthrough
 	case "ppc64le":
